65fec9e3750eef4b65cb8912d4210548c48e8d1d
VentoGITHUB <149194832+VentoGITHUB@users.noreply.github.com>
2025-05-07 19:55:49 +0000

    zmiana sheet.lua i settings.json

--- sheet.lua
+++ sheet.lua
@@ -1,1 +1,37 @@
-print("To sheet :0")
+math.randomseed(os.time())
+
+-- Inicjalizacja 5 tablic 5-elementowych
+array1 = {0, 0, 0, 0, 0}
+array2 = {0, 0, 0, 0, 0}
+array3 = {0, 0, 0, 0, 0}
+array4 = {0, 0, 0, 0, 0}
+array5 = {0, 0, 0, 0, 0}
+
+-- Pomocnicza funkcja do losowania i drukowania tablicy
+function fillAndPrint(array)
+    for i = 1, 5 do
+        array[i] = math.random(0, 99)
+    end
+    io.write("[")
+    for i = 1, 5 do
+        io.write(array[i])
+        if i < 5 then
+            io.write("] [")
+        else
+            io.write("]")
+        end
+    end
+    print() -- nowa linia
+end
+
+for i = 1, 5 do
+    if i > 1 then
+        print("--------------------")
+    end
+
+    fillAndPrint(array1)
+    fillAndPrint(array2)
+    fillAndPrint(array3)
+    fillAndPrint(array4)
+    fillAndPrint(array5)
+end
